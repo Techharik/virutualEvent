c93e8cd675a8d1e228d2abf344e8b07ce55fdc3c
Hari Prasath <khariprasath30@gmail.com>
2025-11-28 20:11:41 +0530

    added the rate Limiter 2

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ export const app = express();
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
     ipv6Subnet: 56,
 })
 
